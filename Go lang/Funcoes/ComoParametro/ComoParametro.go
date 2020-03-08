57f45c13a5cb88b445c1223a0522a218f5aea947
eduardollanda <eduardohollanda14@gmail.com>
2020-03-08 14:14:49 -0300

    tentando entender o por que passar um funcao como parametro

--- Go lang/Funcoes/ComoParametro/ComoParametro.go
+++ Go lang/Funcoes/ComoParametro/ComoParametro.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
+import (
+	"fmt"
+)
 
+func multiplicacao(a, b int) int {
+	return a * b
+}
+
+func exec(funcao func(int, int) int, p1, p2 int) int {
+	return funcao(p1, p2)
+}
+
+func main() {
+	resultado := exec(multiplicacao, 3, 4)
+	fmt.Println(resultado)
 }
